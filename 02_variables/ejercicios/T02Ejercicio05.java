a91aa324ddff0eda6a983486551d4516937e6c5d
miguelga2004 <mgaralc807e@g.educaand.es>
2023-10-25 20:48:46 +0200

    Ejercicio 5

--- 02_variables/ejercicios/T02Ejercicio05.java
+++ 02_variables/ejercicios/T02Ejercicio05.java
@@ -1,9 +1,9 @@
 public class T02Ejercicio05 {
   public static void main(String[] args) {
     int pesetas = 1164;
-    int euros = (int)(pesetas / 166.386);
+    double euros = (pesetas / 166.386);
 
-    System.out.println(euros);
-    
+    System.out.println("De pta a € --> " + euros);
+    System.out.printf("%d pta --> %.2f € \n", pesetas, euros);
   }
 }
