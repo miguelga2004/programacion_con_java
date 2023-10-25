584031f15383cedd2218f8def8d25d0dbe6eb274
miguelga2004 <mgaralc807e@g.educaand.es>
2023-10-25 21:06:21 +0200

    Ejercicio T02 terminados

---
+++ 02_variables/ejercicios/T02Ejercicio07.java
@@ -0,0 +1,11 @@
+public class T02Ejercicio07 {
+  public static void main(String[] args) {
+    String nombre = "Miguel";
+    char apellido = 'G';
+
+    System.out.println(nombre + apellido);
+
+    //No permite convinar char y string    
+  }
+  
+}
---
+++ 02_variables/ejercicios/T02Ejercicio08.java
@@ -0,0 +1,15 @@
+public class T02Ejercicio08 {
+  public static void main(String[] args) {
+    char primero = 'c';
+    char segundo = 'a';
+    char tercero = 's';
+    char cuarto = 'a';
+    char quinto = 's';
+    String cadena = " feas";
+    
+    System.out.println("" + primero + segundo + tercero + cuarto + quinto + cadena);
+
+    //El problema era que hay que colocar "" al principio del println para que no muestre el codigo asci de las letras.
+
+  }
+}
