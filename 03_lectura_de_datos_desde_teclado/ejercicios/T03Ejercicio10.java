09f15ab1c6ecc5f78ac22f302e036dff804fc49e
miguelga2004 <mgaralc807e@g.educaand.es>
2023-11-01 18:04:08 +0100

    Ejercicios tema 3

---
+++ 03_lectura_de_datos_desde_teclado/ejercicios/T03Ejercicio10.java
@@ -0,0 +1,22 @@
+/**
+ * Explica tu codigo aqui
+ * @author Miguel Garcia
+ */
+import java.util.Scanner;
+public class T03Ejercicio10 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int megabite;
+    int resultado;
+   
+
+    System.out.print("Introduce la cantidad de Megabits: ");
+    megabite = sc.nextInt();
+
+    resultado = megabite*1000;
+
+    System.out.println("El resultado de pasar " + megabite + "Mb a Kb es = " + resultado + "Kb");
+    
+    sc .close();
+  }
+}
