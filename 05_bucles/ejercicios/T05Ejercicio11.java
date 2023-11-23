621b445f93bc2c2e8d6ab61de79a06faf407deb7
miguelga2004 <mgaralc807e@g.educaand.es>
2023-11-23 20:17:20 +0100

    t05 11 y 12

--- 05_bucles/ejercicios/T05Ejercicio11.java
+++ 05_bucles/ejercicios/T05Ejercicio11.java
@@ -6,24 +6,25 @@
  * @author Miguel Garcia
  */ 
 import java.util.Scanner;
-public class T05Ejercicio10 {
+public class T05Ejercicio11 {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     double numIntroducido = 0;
-    double media = 0;
-    double suma = 0;
+  
+    System.out.print("Introduce numero: ");
+    numIntroducido = sc.nextInt();
 
-    while (numIntroducido >= 0) {
-      System.out.println("Introduce numero");
-      numIntroducido = sc.nextDouble();
-      if (numIntroducido>=0) {
-        suma = suma + numIntroducido;
-        media ++;
-      }
+    double cuadrado = Math.pow(numIntroducido, 2);
+    double cubo = Math.pow(numIntroducido, 3);
+
+    for (int i = 1; i <= 5; i++) {
+      System.out.printf("%4.0f %6.0f %6.0f\n", numIntroducido, cuadrado, cubo );
+      numIntroducido ++;
+
+      cuadrado = Math.pow(numIntroducido, 2);
+      cubo = Math.pow(numIntroducido, 3);
     }
-    double resultado = suma/media;
-
-    System.out.println("El resultado es " + resultado);
+    
     sc.close();
   }
 }
