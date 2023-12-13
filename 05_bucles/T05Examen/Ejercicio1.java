5b1cb55763060d84c4e5b3f73a67038336b3f050
miguelga2004 <mgaralc807e@g.educaand.es>
2023-12-13 13:09:28 +0100

    t05ejercicios

---
+++ 05_bucles/T05Examen/Ejercicio1.java
@@ -0,0 +1,13 @@
+/**
+ * Explica tu codigo aqui
+ * @author Miguel Garcia
+ */
+import java.util.Scanner;
+public class Ejercicio1 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Introduce la contraseña: ");
+    int num = sc.nextInt();
+      sc.close();
+  }
+}
