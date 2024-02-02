904b178c103a95892af5c788ca4843806686fee8
miguelga2004 <mgaralc807e@g.educaand.es>
2024-02-02 20:30:16 +0100

    gato

--- 09_POO/ejemplos/Animal/Gato.java
+++ 09_POO/ejemplos/Animal/Gato.java
@@ -11,12 +11,47 @@ public class Gato extends Animal {
     raza = "siames";
   }
 
-  public Gato(String raza) {
+  public Gato(String r) {
     super(Sexo.HEMBRA);
-    this.raza = raza;
+    raza = r;
   }
 
+  public Gato(){
+    super(Sexo.HEMBRA);
+    raza = "siamés";
+  }
+
+  public String toString() {
+    return super.toString() + "Raza: " + this.raza + "\n************";
+  }
+
+  public void maullar() {
+    System.out.println("Miauuuuu");
+  }
   
+  public void ronronea() {
+    System.out.println("mrrrrrrr");
+  }
+
+  public void come(String comida) {
+    if (comida.equals("pescado")) {
+      System.out.println("Hmmmm, gracias");
+    } else {
+      System.out.println("Lo siento, yo solo como pescado");
+    }
+  }
+
+  public void peleaCon(Gato contrincante) {
+    if (this.getSexo() == Sexo.HEMBRA) {
+      System.out.println("No me gusta pelear");
+    } else {
+      if (contrincante.getSexo() == Sexo.HEMBRA) {
+        System.out.println("no me gusta pelear");
+      } else {
+        System.out.println("ven aqui que te vas a enterar");
+      }      
+    }
+  }
 
   
   
